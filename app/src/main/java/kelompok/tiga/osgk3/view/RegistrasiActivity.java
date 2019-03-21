098f10f6e930bee595cad8ef1cb4849b15c19456
Lukmanul Hakim <locktis.man@gmail.com>
2019-03-21 11:04:50 +0700

    gitignore

---
+++ app/src/main/java/kelompok/tiga/osgk3/view/RegistrasiActivity.java
@@ -0,0 +1,15 @@
+package kelompok.tiga.osgk3.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import kelompok.tiga.osgk3.R;
+
+import android.os.Bundle;
+
+public class RegistrasiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrasi);
+    }
+}
