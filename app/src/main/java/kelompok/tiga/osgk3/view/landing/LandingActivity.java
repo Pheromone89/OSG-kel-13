cec2807b5fbfd138a283a92da0ee18fb786dff58
Lukmanul Hakim <locktis.man@gmail.com>
2019-03-21 15:13:43 +0700

    landing validasi

--- app/src/main/java/kelompok/tiga/osgk3/view/landing/LandingActivity.java
+++ app/src/main/java/kelompok/tiga/osgk3/view/landing/LandingActivity.java
@@ -1,7 +1,8 @@
 package kelompok.tiga.osgk3.view.landing;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
@@ -9,19 +10,24 @@ import com.google.android.material.textfield.TextInputEditText;
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
+import kelompok.tiga.osgk3.MainActivity;
 import kelompok.tiga.osgk3.R;
+import kelompok.tiga.osgk3.model.Login;
+import kelompok.tiga.osgk3.viewmodel.LandingViewModel;
 
-public class LandingActivity extends AppCompatActivity {
+public class LandingActivity extends AppCompatActivity implements NavigatorLanding {
 
     @BindView(R.id.landingEditTextUsername)
-    TextInputEditText landingEditTextUsername;
+    TextInputEditText edtUsername;
     @BindView(R.id.landingEditTextPassword)
-    TextInputEditText landingEditTextPassword;
+    TextInputEditText edtPassword;
     @BindView(R.id.landingButtonLogin)
-    MaterialButton landingButtonLogin;
+    MaterialButton btnLogin;
     @BindView(R.id.landingButtonRegistrasi)
-    MaterialButton landingButtonRegistrasi;
+    MaterialButton btnRegistrasi;
+
+    private LandingViewModel viewModel;
+    private Login login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,28 +35,35 @@ public class LandingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_landing);
         ButterKnife.bind(this);
 
+        login = new Login();
+        viewModel = new LandingViewModel(this, login);
+        viewModel.setNavigator(this);
 
+
+        btnLogin.setOnClickListener((v)->{
+            viewModel.updateModel(edtUsername.getText().toString(), edtPassword.getText().toString());
+            viewModel.validLogin();
+        });
+
+        btnRegistrasi.setOnClickListener((v -> {
+            viewModel.updateModel(edtUsername.getText().toString(), edtPassword.getText().toString());
+            viewModel.validRegistrasi();
+        }));
+    }
+
+    @Override
+    public void onSuccess() {
+        showToast("BERHASIL LOGIN");
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    @Override
+    public void onFailed(String message) {
+        showToast(message);
     }
 
 
-
-    void validasi (){
-
-    }
-
-
-
-
-
-    @OnClick({R.id.landingButtonLogin, R.id.landingButtonRegistrasi})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.landingButtonLogin:
-
-                break;
-            case R.id.landingButtonRegistrasi:
-
-                break;
-        }
+    private void showToast(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
